10167e3eac2a78656557716a835d9b27d8a34ee4
Laura <laura.coombs07@gmail.com>
2019-01-18 16:01:11 -0700

    FULL CRUD

--- Presentations.js
+++ Presentations.js
@@ -24,35 +24,36 @@ app.get("/presentations", async function(req, res) {
   res.status(200).send(result);
 });
 
-app.get("/presentations/:name", async function(req, res){ 
-  const result = await presentations.find({"presenter": req.params.name});//.find({},{limit:20},function(e,docs)){
-    //     res.json(docs);
-    //   })
-    // if (!presentation) res.status(404).send("This Presenter cannot be found.");
-  res.status(200).send(result);
-});
+// app.get("/presentations/:name", async (req, res) => { 
+//   const result = await presentations.find({"presenter": req.params.name});//.find({},{limit:20},function(e,docs)){
+//     //     res.json(docs);
+//     //   })
+//     // if (!presentation) res.status(404).send("This Presenter cannot be found.");
+//   res.status(200).send(result);
+// });
 
-app.get('/presentations/:id', async (req, res) => { 
-     await presentations.find({}, (err, presentations) => {
-      res.json(presentations)
+app.get('/presentations/:_id', async (req, res) => { 
+     const result = await presentations.findOne({"_id": req.params._id});
+     console.log(result)
+      res.status(200).send(result);
+    //  res.json(presentations).send(result);
      })
-});
+// });
 //   // POST method route
 app.post("/presentations", function(req, res) {
   const result = presentations.insert(req.body);
-  res.status(200).send(result);
+  res.status(200).send(result);//express
 });
 
 // PUT OR PATCH method Route
 
-app.put("/presentation/:_id", async (req, res) => {
-  console.log(req.params._id)
-  await presentation.update(req.params._id);
-  // if (!presentation) res.status(404).send("This Presentation cannot be found.");
-  // const { error }
-  presentation.presenter = req.body.presenter;
-  res.send(presentation);
-  //res.status(200).send(result)
+app.put("/presentations/:_id", async (req, res) => {//express (just changed to :id instead of :_id)
+  const update = await presentations.findOneAndUpdate({"_id": req.params._id },  req.body)
+  // presentation.set({ presenter: ''});
+  // presentation.save()
+// await presentations.update({ presenter: ''}, { $set: {mpresenter: ''}} 
+// )
+ res.status(200).send(update)
 });
 
 app.delete("/presentations/:_id", async function (req, res) {
@@ -65,3 +66,8 @@ app.delete("/presentations/:_id", async function (req, res) {
 
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+  // await presentations.update(req.params._id);
+  // if (!presentation) res.status(404).send("This Presentation cannot be found.");
+  // const { error }
+  // presentations.presenter = req.body.presenter;
+  // res.send(presentation);//express
